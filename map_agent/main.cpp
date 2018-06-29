df6ae6b29df326041d5183819f27e2e90e680f3c
Dave Kennedy <davepkennedy@gmail.com>
2018-06-29 21:06:41 +0100

    Initial Commit

---
+++ map_agent/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  map_agent
+//
+//  Created by Dave Kennedy on 29/06/2018.
+//  Copyright © 2018 Dave Kennedy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
